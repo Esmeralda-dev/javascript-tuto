ee4fee8eeed0af290d6ccf23155e5806a2f04c51
Esma Avci <esmac@hotmail.fr>
2025-04-24 22:35:38 +0200

    bases

---
+++ main.js
@@ -0,0 +1,68 @@
+// //
+
+// let motUtilisateur = prompt("Enterz un mot");
+// console.log(motUtilisateur);
+
+// if (motUtilisateur) {
+//   console.log("vous avez correctement taper le mot");
+// }
+
+// const listeMots = ["Cachalot", "Petunia", "Serviette"];
+// let score = 0;
+
+// let motUtilisateur = prompt("Entrer le mot" + " " + listeMots[0]);
+
+// if (motUtilisateur === listeMots[0]) {
+//   score++;
+// } else {
+//   console.log("hatali");
+// }
+
+// motUtilisateur = prompt("Entrer le mot" + " " + listeMots[1]);
+// if (motUtilisateur === listeMots[1]) {
+//   score++;
+// } else {
+//   console.log("hata var 2");
+// }
+
+// motUtilisateur = prompt("Entrer le mot" + " " + listeMots[2]);
+// if (motUtilisateur === listeMots[2]) {
+//   score++;
+// } else {
+//   console.log("hata var 3");
+// }
+
+// console.log(score);
+
+// let listeMots = ["petunia", "cachalot", "serviette"];
+// let motUtilisateur = prompt("entrer le mot:" + listeMots);
+
+// for (listeMots = 0; listeMots < 3; listeMots++) {
+//   console.log(listeMots);
+// }
+
+// for (i = 1; i <= 15; i++) {
+//   console.log(i);
+// }
+
+// var markalar = ["mazda", "opel", "bmw", "toyota", "toyota 2", "toyota 3"];
+
+// for (i = 0; i < markalar.length; i++) {
+//   console.log(markalar[i]);
+// }
+
+// let people = [
+//   { firstName: "Ada", lastName: "Bilgi" },
+//   { firstName: "Yiğit", lastName: "Bilgi" },
+//   { firstName: "Çınar", lastName: "Turan" },
+//   { firstName: "Esma", lastName: "Uyuz" },
+// ];
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i].firstName + " " + people[i].lastName);
+// }
+let listeMots = ["Cachalot", "petunia", "serviette"];
+let motUtilisateur = prompt("entrer le mot:" + listeMots[0]);
+for (let i = 0; i < listeMots.length; i++) {
+  console.log(motUtilisateur[i]);
+}
